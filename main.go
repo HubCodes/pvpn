0c56de866da8af00c600900b7a5cae8b7bfe5a3a
HubCodes <krlrhkstk12@gmail.com>
2020-05-24 19:25:02 +0900

    이것은 클라이언트

--- main.go
+++ main.go
@@ -14,22 +14,122 @@ import (
 	"github.com/songgao/water"
 )
 
-const mtu = "1500"
+type config struct {
+	LocalIP    string
+	RemoteIP   string
+	Port       uint16
+	IsClient   bool
+	MTU        string
+	BufferSize uint
+}
 
-var (
-	localIP  = flag.String("local", "", "Local tun interface IP/MASK like 192.168.3.3/24")
-	remoteIP = flag.String("remote", "", "Remote server (external) IP like 8.8.8.8")
-	port     = flag.Int("port", 4321, "UDP port for communication")
-	isClient = flag.Bool("is-client", true, "Run as client?")
-)
+type context struct {
+	conf *config
+	tun  *water.Interface
+}
+
+const bufferSize = 1500
 
 func main() {
-	flag.Parse()
+	conf := parseConf()
 	tun := createTUN()
-	updateRoutingTable(tun.Name(), *remoteIP)
+	updateRoutingTable(tun.Name(), &conf)
+	registerSignalHandler(conf.RemoteIP)
+	if conf.IsClient {
+		client(&context{conf: &conf, tun: tun})
+	} else {
+		server(&context{conf: &conf, tun: tun})
+	}
+	listenAddr, err := net.ResolveUDPAddr("udp4", fmt.Sprintf("%s:%v", conf.RemoteIP, conf.Port))
+	if err != nil {
+		log.Panicln("Cannot resolve server address:", err)
+	}
+	listen, err := net.ListenUDP("udp4", listenAddr)
+	if err != nil {
+		log.Panicln("Cannot listen:", err)
+	}
+	defer listen.Close()
+	go func() {
+		buffer := make([]byte, 2048)
+		for {
+			n, err := tun.Read(buffer)
+			if err != nil {
+				log.Panicln("Cannot read from buffer:", err)
+			}
+			listen.WriteToUDP(buffer, listenAddr)
+		}
+	}()
+	packet := make([]byte, 2000)
+	for {
+		n, err := tun.Read(packet)
+		if err != nil {
+			log.Panicln("Error while reading from TUN device:", err)
+		}
+
+	}
+}
+
+func parseConf() config {
+	var (
+		localIP  = flag.String("local", "", "Local tun interface IP/MASK like 192.168.3.3/24")
+		remoteIP = flag.String("remote", "", "Remote server (external) IP like 8.8.8.8")
+		port     = flag.Uint("port", 4321, "UDP port for communication")
+		isClient = flag.Bool("is-client", true, "Run as client?")
+	)
+	flag.Parse()
+	conf := config{
+		LocalIP:  *localIP,
+		RemoteIP: *remoteIP,
+		Port:     uint16(*port),
+		IsClient: *isClient,
+		MTU:      "1500"}
+	return conf
+}
+
+func createTUN() *water.Interface {
+	ifce, err := water.New(water.Config{
+		DeviceType: water.TUN,
+	})
+	if err != nil {
+		log.Panicln("Failed to create TUN interface:", err)
+	}
+	log.Println("TUN interface created with name:", ifce.Name())
+	return ifce
+}
+
+func updateRoutingTable(name string, conf *config) {
+	localTUNIP := "10.8.0.1"
+	err := exec.Command("ifconfig", name, localTUNIP, "mtu", conf.MTU, "up").Run()
+	if err != nil {
+		log.Panicln("Cannot bind IP address of TUN interface:", name)
+	}
+	err = exec.Command("sysctl", "-w", "net.inet.ip.forwarding=1").Run()
+	if err != nil {
+		log.Panicln("Cannot change sysctl ip forwarding preference")
+	}
+	defaultGatewayIP := getDefaultGatewayIP(name)
+	err = exec.Command("route", "add", conf.RemoteIP, defaultGatewayIP).Run()
+	if err != nil {
+		log.Panicln("Cannot add route info:", err)
+	}
+	err = exec.Command("route", "add", "0/1", localTUNIP).Run()
+	if err != nil {
+		log.Panicln("Cannot assign routing rule:", err)
+	}
+	err = exec.Command("route", "add", "128/1", localTUNIP).Run()
+	if err != nil {
+		log.Panicln("Cannot assign routing rule:", err)
+	}
+}
+
+func registerSignalHandler(remoteIP string) {
 	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-	isExit := make(chan bool, 1)
+	signal.Notify(
+		sigChan,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
 	go func() {
 		for {
 			s := <-sigChan
@@ -42,74 +142,11 @@ func main() {
 				fallthrough
 			case syscall.SIGINT:
 				log.Println("goodbye")
-				restoreRoutingTable(*remoteIP)
-				isExit <- true
+				restoreRoutingTable(remoteIP)
+				os.Exit(0)
 			}
 		}
 	}()
-	listenAddr, err := net.ResolveUDPAddr("udp4", fmt.Sprintf("%s:%v", *remoteIP, *port))
-	if err != nil {
-		log.Fatalln("Cannot resolve server address:", err)
-	}
-	listen, err := net.ListenUDP("udp4", listenAddr)
-	if err != nil {
-		log.Fatalln("Cannot listen:", err)
-	}
-	defer listen.Close()
-	go func() {
-		buffer := make([]byte, 2048)
-		for {
-			n, err := tun.Read(buffer)
-			if err != nil {
-				log.Fatalln("Cannot read from buffer:", err)
-			}
-			listen.WriteToUDP(buffer, listenAddr)
-		}
-	}()
-	packet := make([]byte, 2000)
-	for {
-		n, err := tun.Read(packet)
-		if err != nil {
-			log.Fatalln("Error while reading from TUN device:", err)
-		}
-
-	}
-}
-
-func createTUN() *water.Interface {
-	ifce, err := water.New(water.Config{
-		DeviceType: water.TUN,
-	})
-	if err != nil {
-		log.Fatalln("Failed to create TUN interface:", err)
-	}
-	log.Println("TUN interface created with name:", ifce.Name())
-	return ifce
-}
-
-func updateRoutingTable(name string, external string) {
-	remoteTUNIP := "10.8.0.1"
-	err := exec.Command("ifconfig", name, remoteTUNIP, "mtu", mtu, "up").Run()
-	if err != nil {
-		log.Fatalln("Cannot bind IP address of TUN interface:", name)
-	}
-	err = exec.Command("sysctl", "-w", "net.inet.ip.forwarding=1").Run()
-	if err != nil {
-		log.Fatalln("Cannot change sysctl ip forwarding preference")
-	}
-	defaultGatewayIP := getDefaultGatewayIP(name, mtu)
-	err = exec.Command("route", "add", external, defaultGatewayIP).Run()
-	if err != nil {
-		log.Fatalln("Cannot add route info:", err)
-	}
-	err = exec.Command("route", "add", "0/1", remoteTUNIP).Run()
-	if err != nil {
-		log.Fatalln("Cannot assign routing rule:", err)
-	}
-	err = exec.Command("route", "add", "128/1", remoteTUNIP).Run()
-	if err != nil {
-		log.Fatalln("Cannot assign routing rule:", err)
-	}
 }
 
 func restoreRoutingTable(external string) {
@@ -118,14 +155,49 @@ func restoreRoutingTable(external string) {
 	exec.Command("route", "delete", "128/1")
 }
 
-func getDefaultGatewayIP(name string, mtu string) string {
+func getDefaultGatewayIP(name string) string {
 	getDefaultRoute := "route -n get default 2>/dev/null"
 	re := regexp.MustCompile("?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)")
 	out, err := exec.Command("sh", "-c", getDefaultRoute).Output()
 	if err != nil {
-		log.Fatalln("Error while reading default route info:", err)
+		log.Panicln("Error while reading default route info:", err)
 	}
 	outStr := string(out)
 	match := re.FindStringSubmatch(outStr)[0]
 	return match
+}
+
+func client(ctxt *context) {
+	tun := ctxt.tun
+	conf := ctxt.conf
+	remote := conf.RemoteIP
+	port := conf.Port
+	remoteAddrStr := fmt.Sprintf("%s:%d", remote, port)
+	remoteAddr, err := net.ResolveUDPAddr("udp", remoteAddrStr)
+	if err != nil {
+		log.Panicln("Cannot resolve server address:", remoteAddrStr)
+	}
+	conn, err := net.DialUDP("udp", nil, remoteAddr)
+	if err != nil {
+		log.Panicln("Cannot dial server:", err)
+	}
+	buf := make([]byte, bufferSize)
+	for {
+		n, err := tun.Read(buf)
+		if err != nil {
+			log.Panicln("Cannot read from TUN device")
+		}
+		// TODO: 여기서 암호화 진행하기
+		for n > 0 {
+			written, err := conn.Write(buf)
+			if err != nil {
+				log.Panicln("Cannot write to server")
+			}
+			n -= written
+		}
+	}
+}
+
+func server(ctxt *context) {
+
 }
