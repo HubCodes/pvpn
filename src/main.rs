f93d5a50288a4434afeb92b1ed3a74f00a39855a
HubCodes <krlrhkstk12@gmail.com>
2020-05-28 19:19:48 +0900

    라우팅 테이블을 바꾸자

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 extern crate tun_tap;
 
-use std::process::{Command, Child};
-use std::io::Result;
+use std::process::{Command};
 use tun_tap::{Iface, Mode};
 
 const MTU: u32 = 1500;
+const SERVER_HOST: &str = "";
 
 fn main() {
     let is_client = true;
@@ -13,26 +13,34 @@ fn main() {
     setup_route_table(is_client);
 }
 
-fn run(cmd: &str, args: &[&str]) -> Result<Child> {
-    Command::new(cmd).args(args).spawn()
+fn run(cmd: &str) {
+    Command::new("sh").arg("-c").arg(cmd).spawn().unwrap();
 }
 
 fn ifconfig(mtu: u32, is_client: bool) {
     if is_client {
-        run("ifconfig", &["tun0", "10.8.0.2/16", "mtu", &mtu.to_string(), "up"])
-        .expect("Cannot assign IP of TUN device");
+        run(&format!("ifconfig tun0 10.8.0.2/16 mtu {} up", mtu));
     } else {
-        run("ifconfig", &["tun0", "10.8.0.1/16", "mtu", &mtu.to_string(), "up"])
-        .expect("Cannot assign IP of TUN device");
+        run(&format!("ifconfig tun0 10.8.0.1/16 mtu {} up", mtu));
     }
 }
 
 fn setup_route_table(is_client: bool) {
-    run("sysctl", &["-w", "net.ipv4.ip_forward=1"])
-    .expect("Cannot change ip forward preference");
+    run("sysctl -w net.ipv4.ip_forward=1");
     if is_client {
-
+        run("iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE");
+        run("iptables -I FORWARD 1 -i tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT");
+        run("iptables -I FORWARD 1 -o tun0 -j ACCEPT");
+        run(&format!("ip route add {} via $(ip route show 0/0 | sed -e 's/.* via \([^ ]*\).*/\1/')", SERVER_HOST));
+        run("ip route add 0/1 dev tun0");
+        run("ip route add 128/1 dev tun0");
     } else {
-
+        run("iptables -t nat -A POSTROUTING -s 10.8.0.0/16 ! -d 10.8.0.0/16 -m comment --comment 'vpndemo' -j MASQUERADE");
+        run("iptables -A FORWARD -s 10.8.0.0/16 -m state --state RELATED,ESTABLISHED -j ACCEPT");
+        run("iptables -A FORWARD -d 10.8.0.0/16 -j ACCEPT");
     }
+}
+
+fn cleanup_when_sig_exit(is_client: bool) {
+    
 }
